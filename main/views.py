51c0fad06e0baa36b63c018d1ef0e988158898ea
shanahandnlf <shanahan.danualif@ui.ac.id>
2023-09-11 12:48:30 +0700

    coba

--- main/views.py
+++ main/views.py
@@ -4,13 +4,8 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'app_name': 'main',
+        'app_name': 'HanShop',
         'nama_mahasiswa': 'Shanahan Danualif Erwin',
         'class_name': 'PBP F',
-
-        # 'name': 'Holo Charizard Card',
-        # 'amount': '100',
-        # 'description': 'Authentic from 1999',
-        # 'price': '$4200'
     }
     return render(request, "main.html", context)
